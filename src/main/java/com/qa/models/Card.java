5ac021f90b97b0c33431d00749c4e380435030aa
Jonathan Humphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-09 15:20:17 +0000

    created a card entitity

--- src/main/java/com/qa/models/Card.java
+++ src/main/java/com/qa/models/Card.java
@@ -1,4 +1,43 @@
 package com.qa.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String name;
+    String clan;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClan() {
+        return clan;
+    }
+
+    public void setClan(String clan) {
+        this.clan = clan;
+    }
+
+
 }
