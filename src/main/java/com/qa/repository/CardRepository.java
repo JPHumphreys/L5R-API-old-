b4320e84e90c1ec9b02d0bdd743d49469dc51b6e
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-19 10:39:59 +0000

    added list import

--- src/main/java/com/qa/repository/CardRepository.java
+++ src/main/java/com/qa/repository/CardRepository.java
@@ -2,6 +2,7 @@ package com.qa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.qa.models.Card;
+import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card, String>{
 
