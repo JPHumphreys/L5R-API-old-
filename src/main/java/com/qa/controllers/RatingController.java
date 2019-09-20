d1565fd2e38e1b2af685cce2e1fb0630cb938d70
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-20 21:01:34 +0100

    added a overallrating function

--- src/main/java/com/qa/controllers/RatingController.java
+++ src/main/java/com/qa/controllers/RatingController.java
@@ -25,7 +25,7 @@ public class RatingController {
     }
 
     @RequestMapping(value = "add/ratings/{clan}/{id}",method = RequestMethod.PUT)
-    public Rating addRatingVSCrab(@PathVariable String id,@PathVariable String clan, @RequestBody Rating rating){
+    public Rating addRatingVSCrab(@PathVariable String clan, @PathVariable String id,@RequestBody Rating rating){
         Rating card = repository.findOne(id);
 
         switch (clan){
@@ -37,6 +37,7 @@ public class RatingController {
                     card.setRatingcrab(((card.getTotalvotescrab() * card.getRatingcrab()) + rating.getRatingcrab())/(card.getTotalvotescrab() + 1));
                 }
                 card.setTotalvotescrab(card.getTotalvotescrab() + 1);
+                card.setOverallrating(returnNewOverallRating(card));
                 break;
             case "crane":
                 if(card.getTotalvotescrane() == 0 || card.getRatingcrane() == 0){
@@ -46,6 +47,7 @@ public class RatingController {
                     card.setRatingcrane(((card.getTotalvotescrane() * card.getRatingcrane()) + rating.getRatingcrane())/(card.getTotalvotescrane() + 1));
                 }
                 card.setTotalvotescrane(card.getTotalvotescrane() + 1);
+                card.setOverallrating(returnNewOverallRating(card));
                 break;
             case "dragon":
                 if(card.getTotalvotesdragon() == 0 || card.getRatingdragon() == 0){
@@ -55,6 +57,7 @@ public class RatingController {
                     card.setRatingdragon(((card.getTotalvotesdragon() * card.getRatingdragon()) + rating.getRatingdragon())/(card.getTotalvotesdragon() + 1));
                 }
                 card.setTotalvotesdragon(card.getTotalvotesdragon() + 1);
+                card.setOverallrating(returnNewOverallRating(card));
                 break;
             case "lion":
                 if(card.getTotalvoteslion() == 0 || card.getRatinglion() == 0){
@@ -64,6 +67,7 @@ public class RatingController {
                     card.setRatinglion(((card.getTotalvoteslion() * card.getRatinglion()) + rating.getRatinglion())/(card.getTotalvoteslion() + 1));
                 }
                 card.setTotalvoteslion(card.getTotalvoteslion() + 1);
+                card.setOverallrating(returnNewOverallRating(card));
                 break;
             case "phoenix":
                 if(card.getTotalvotesphoenix() == 0 || card.getRatingphoenix() == 0){
@@ -73,6 +77,7 @@ public class RatingController {
                     card.setRatingphoenix(((card.getTotalvotesphoenix() * card.getRatingphoenix()) + rating.getRatingphoenix())/(card.getTotalvotesphoenix() + 1));
                 }
                 card.setTotalvotesphoenix(card.getTotalvotesphoenix() + 1);
+                card.setOverallrating(returnNewOverallRating(card));
                 break;
             case "scorpion":
                 if(card.getTotalvotesscorpion() == 0 || card.getRatingscorpion() == 0){
@@ -82,6 +87,7 @@ public class RatingController {
                     card.setRatingscorpion(((card.getTotalvotesscorpion() * card.getRatingscorpion()) + rating.getRatingscorpion())/(card.getTotalvotesscorpion() + 1));
                 }
                 card.setTotalvotesscorpion(card.getTotalvotesscorpion() + 1);
+                card.setOverallrating(returnNewOverallRating(card));
                 break;
             case "unicorn":
                 if(card.getTotalvotesunicorn() == 0 || card.getRatingunicorn() == 0){
@@ -91,29 +97,122 @@ public class RatingController {
                     card.setRatingunicorn(((card.getTotalvotesunicorn() * card.getRatingunicorn()) + rating.getRatingunicorn())/(card.getTotalvotesunicorn() + 1));
                 }
                 card.setTotalvotesunicorn(card.getTotalvotesunicorn() + 1);
+                card.setOverallrating(returnNewOverallRating(card));
                 break;
         }
         repository.saveAndFlush(card);
         return card;
     }
 
-    @RequestMapping(value = "remove/ratings/crab/{id}",method = RequestMethod.PUT)
-    public Rating removeRatingVSCrab(@PathVariable String id, @RequestBody Rating rating){
+    @RequestMapping(value = "remove/ratings/{clan}/{id}",method = RequestMethod.PUT)
+    public Rating removeRatingVSCrab(@PathVariable String clan, @PathVariable String id, @RequestBody Rating rating){
         Rating card = repository.findOne(id);
 
-        if(card.getTotalvotescrab() <=1){
-            card.setRatingcrab(0);
-            card.setTotalvotescrab(0);
+        switch (clan){
+            case "crab":
+                if(card.getTotalvotescrab() <=1){
+                    card.setRatingcrab(0);
+                    card.setTotalvotescrab(0);
+                }
+                else{
+                    card.setRatingcrab(((card.getTotalvotescrab() * card.getRatingcrab()) - rating.getRatingcrab())/(card.getTotalvotescrab() - 1));
+                    card.setTotalvotescrab(card.getTotalvotescrab() - 1);
+                }
+                card.setOverallrating(returnNewOverallRating(card));
+                break;
+            case "crane":
+                if(card.getTotalvotescrane() <=1){
+                    card.setRatingcrane(0);
+                    card.setTotalvotescrane(0);
+                }
+                else{
+                    card.setRatingcrane(((card.getTotalvotescrane() * card.getRatingcrane()) - rating.getRatingcrane())/(card.getTotalvotescrane() - 1));
+                    card.setTotalvotescrane(card.getTotalvotescrane() - 1);
+                }
+                card.setOverallrating(returnNewOverallRating(card));
+                break;
+            case "dragon":
+                if(card.getTotalvotesdragon() <=1){
+                    card.setRatingdragon(0);
+                    card.setTotalvotesdragon(0);
+                }
+                else{
+                    card.setRatingdragon(((card.getTotalvotesdragon() * card.getRatingdragon()) - rating.getRatingdragon())/(card.getTotalvotesdragon() - 1));
+                    card.setTotalvotesdragon(card.getTotalvotesdragon() - 1);
+                }
+                card.setOverallrating(returnNewOverallRating(card));
+                break;
+            case "lion":
+                if(card.getTotalvoteslion() <=1){
+                    card.setRatinglion(0);
+                    card.setTotalvoteslion(0);
+                }
+                else{
+                    card.setRatinglion(((card.getTotalvoteslion() * card.getRatinglion()) - rating.getRatinglion())/(card.getTotalvoteslion() - 1));
+                    card.setTotalvoteslion(card.getTotalvoteslion() - 1);
+                }
+                card.setOverallrating(returnNewOverallRating(card));
+                break;
+            case "phoenix":
+                if(card.getTotalvotesphoenix() <=1){
+                    card.setRatingphoenix(0);
+                    card.setTotalvotesphoenix(0);
+                }
+                else{
+                    card.setRatingphoenix(((card.getTotalvotesphoenix() * card.getRatingphoenix()) - rating.getRatingphoenix())/(card.getTotalvotesphoenix() - 1));
+                    card.setTotalvotesphoenix(card.getTotalvotesphoenix() - 1);
+                }
+                card.setOverallrating(returnNewOverallRating(card));
+                break;
+            case "scorpion":
+                if(card.getTotalvotesscorpion() <=1){
+                    card.setRatingscorpion(0);
+                    card.setTotalvotesscorpion(0);
+                }
+                else{
+                    card.setRatingscorpion(((card.getTotalvotesscorpion() * card.getRatingscorpion()) - rating.getRatingscorpion())/(card.getTotalvotesscorpion() - 1));
+                    card.setTotalvotesscorpion(card.getTotalvotesscorpion() - 1);
+                }
+                card.setOverallrating(returnNewOverallRating(card));
+                break;
+            case "unicorn":
+                if(card.getTotalvotesunicorn() <=1){
+                    card.setRatingunicorn(0);
+                    card.setTotalvotesunicorn(0);
+                }
+                else{
+                    card.setRatingunicorn(((card.getTotalvotesunicorn() * card.getRatingunicorn()) - rating.getRatingunicorn())/(card.getTotalvotesunicorn() - 1));
+                    card.setTotalvotesunicorn(card.getTotalvotesunicorn() - 1);
+                }
+                card.setOverallrating(returnNewOverallRating(card));
+                break;
         }
-        else{
-            //card.setRatingcrab((card.getRatingcrab() - (rating.getRatingcrab()/card.getTotalvotescrab() - 1)));
-            card.setRatingcrab(((card.getTotalvotescrab() * card.getRatingcrab()) - rating.getRatingcrab())/(card.getTotalvotescrab() - 1));
-            card.setTotalvotescrab(card.getTotalvotescrab() - 1);
-        }
-
         repository.saveAndFlush(card);
         return card;
     }
+
+    public float returnNewOverallRating(Rating obj){
+
+    int totalVotes = obj.getTotalvotescrab() +
+            obj.getTotalvotescrane() +
+            obj.getTotalvotesdragon() +
+            obj.getTotalvoteslion() +
+            obj.getTotalvotesphoenix() +
+            obj.getTotalvotesscorpion() +
+            obj.getTotalvotesunicorn();
+
+    float totalRatings = obj.getRatingcrab() +
+            obj.getRatingcrane() +
+            obj.getRatingdragon() +
+            obj.getRatinglion() +
+            obj.getRatingphoenix() +
+            obj.getRatingscorpion() +
+            obj.getRatingunicorn();
+
+    return (totalRatings / totalVotes);
+
+    }
+
 
 
 
